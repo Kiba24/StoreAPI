d7c829a7df17696a6b1fdb18816882fec862d27d
Kiba24 <81634416+Kiba24@users.noreply.github.com>
2023-08-22 17:39:00 -0300

    Initial Migration

--- Core/Entities/Product.cs
+++ Core/Entities/Product.cs
@@ -1,14 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Core.Entities
 {
+    [Table("Products")]
     public class Product
     {
+        [Key]
         public Guid Id { get; set; }
+        [Required]
+        [MaxLength(255)]
         public string Name { get; set; }
         public decimal Price { get; set; }
         public DateTime CreatedAT { get; set; }
