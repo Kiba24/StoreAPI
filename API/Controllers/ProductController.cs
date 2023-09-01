f77e7db12deeb1bfa36680b56b341c3988da5dd8
Kiba24 <81634416+Kiba24@users.noreply.github.com>
2023-09-01 18:22:09 -0300

    unfinished productController

--- API/Controllers/ProductController.cs
+++ API/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using Core.Interfaces;
+﻿using Core.Dtos.Product;
+using Core.Interfaces;
 using Infrastructure.Unit_Of_Work;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -16,6 +17,24 @@ namespace API.Controllers
             _unitOfWork = unitOfWork;
         }
 
+        [HttpGet]
+        public async Task<IActionResult> GetById(int id)
+        {
+            return Ok(await _unitOfWork.ProductRepository.GetByIdAsync(id));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await _unitOfWork.ProductRepository.GetAllAsync());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Save(AddProductDto product)
+        {
+            //I need to map the current dto to the entity.
+            return Ok(_unitOfWork.ProductRepository.Add());
+        }
 
     }
 }
